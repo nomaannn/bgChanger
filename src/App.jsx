991434b77a7445359672af30753efcbebe0e4fb4
unknown <nomaan123456@gmail.com>
2023-12-26 23:42:32 +0530

    password Generator completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,95 @@
-import { useState } from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react'
 
 import './App.css'
 
 function App() {
-  const [backgroundColor, setBackgroundColor] = useState("lightgrey")
+
+  const [length, setLength] = useState(8)
+  const [Password, setPassword] = useState("")
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
+
+  //useRef hook
+  const passwordRef = useRef(null)
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if (numberAllowed) str += "0123456789"
+    if (charAllowed) str += "!@#$%^&*-_+=[]{}~`"
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass += str.charAt(char)
+
+    }
+
+    setPassword(pass)
+
+
+  }, [length, numberAllowed, charAllowed, setPassword])
+
+
+  useEffect(() => {
+    passwordGenerator()
+  }, [length, numberAllowed, charAllowed, passwordGenerator])
+
+
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 999);
+    window.navigator.clipboard.writeText(Password)
+  }, [Password])
 
   return (
     <>
-      <div className=' w-screen h-screen' style={{ backgroundColor: backgroundColor }}>
-        <button
-          className='bg-red-500 text-white m-2 rounded-md px-4 py-2'
-          onClick={() => setBackgroundColor("red")}
-        >Red</button>
-        <button
-          className='bg-green-500 text-white m-2 rounded-md px-4 py-2'
-          onClick={() => setBackgroundColor("green")}
-        >Green</button>
-        <button
-          className='bg-blue-500 text-white m-2 rounded-md px-4 py-2'
-          onClick={() => setBackgroundColor("blue")}
-        >Blue</button>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3  bg-gray-800 text-orange-500">
+        <h1 className='text-white text-center my-3'>Password generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={Password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="Password"
+            readOnly
 
-        <button
-          className='bg-pink-200  m-2 rounded-md px-4 py-2'
-          onClick={() => setBackgroundColor("pink")}
-        >Pink</button>
-        <button
-          className='bg-green-200 text-white m-2 rounded-md px-4 py-2'
-          onClick={() => setBackgroundColor("lightgreen")}
-        >Light Green</button>
+          />
+          <button
+            onClick={copyPasswordToClipboard}
+            className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+          >copy</button>
+
+        </div>
+        <div className='flex text-sm gap-x-2'>
+          <div className='flex items-center gap-x-1'>
+            <input
+              type="range"
+              min={6}
+              max={100}
+              onChange={(e) => { setLength(e.target.value) }}
+              className='cursor-pointer'
+
+            />
+            <label>Length: {length}</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              onChange={() => setNumberAllowed((prev) => !prev)}
+              id="numberInput"
+
+            />
+            <label htmlFor="numberInput">Numbers</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              onChange={() => setCharAllowed((prev) => !prev)}
+              id="characterInput"
+
+            />
+            <label htmlFor="characterInput">Characters</label>
+          </div>
+        </div>
       </div>
     </>
   )
